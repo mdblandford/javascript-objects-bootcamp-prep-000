d4d9873ac8466dba3fd84ee3bd4d6fee59b47f06
Michael Blandford <mdblandford@gmail.com>
2018-03-26 23:28:15 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,2 +1,2 @@
 var playlist = {artist:'song'};
-function updatePlaylist(playlist,artist,song) {playlist[artist]=song return playlist}
+function updatePlaylist(playlist,artist,song) {playlist[artist+song] return playlist}
